7ba513a0e3bc1f21845c885156150b7dc2d8b30a
unknown <fedra.ka@gmail.com>
2020-10-19 11:23:39 -0400

    first commit

---
+++ assignment1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nType a string of numbers and seprate them by '+' then press Enter. ");
+            string strInput = Console.ReadLine();
+            int sum=0;
+
+            int[] arrInput = Array.ConvertAll(strInput.Split('+'), int.Parse);
+            foreach(int x in arrInput)
+            {
+                sum += x;
+            }
+            Console.WriteLine(sum);
+           
+        }
+    }
+}
